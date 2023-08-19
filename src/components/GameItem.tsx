45beeef0e19f719496c310a07b227d3c1ad6266f
mukulbbhardwaj <themukulbhardwaj@example.com>
2023-08-20 00:44:22 +0530

    types error handled

--- src/components/GameItem.tsx
+++ src/components/GameItem.tsx
@@ -1,10 +1,16 @@
-const GameItem = ({ img,projectLink}) => {
+
+
+type Props = {
+  img: string;
+  projectLink: string;
+};
+
+const GameItem = ({ img, projectLink }:Props) => {
   return (
-      <div className="gameItem ">
-          <a href={projectLink} target="_blank">
-              
-      <img className="gameIcon" src={img} alt="" />
-          </a>
+    <div className="gameItem ">
+      <a href={projectLink} target="_blank">
+        <img className="gameIcon" src={img} alt="" />
+      </a>
     </div>
   );
 };
